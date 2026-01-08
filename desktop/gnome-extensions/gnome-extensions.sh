e4ec722d56b3f3590829f27a680e753c005d068c
hainet50b <20400616+hainet50b@users.noreply.github.com>
2026-01-08 22:14:59 +0900

    Fix output path to be relative to script path

--- desktop/gnome-extensions/gnome-extensions.sh
+++ desktop/gnome-extensions/gnome-extensions.sh
@@ -1,1 +1,9 @@
-gnome-extensions list --enabled | sort > gnome-extensions.txt
+#!/usr/bin/env bash
+set -e
+
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+
+gnome-extensions list --enabled \
+  | sort \
+  > "${script_dir}/gnome-extensions.txt"
+
