c75f0f113b042afee09b526ab5881dba9b7f621e
hainet50b <20400616+hainet50b@users.noreply.github.com>
2026-02-02 16:25:27 +0900

    Add desktop entry installation to VSCodium install script

--- desktop/vscodium.sh
+++ desktop/vscodium.sh
@@ -13,41 +13,55 @@ releases=$(curl -fsSL \
 latest_version=$(echo "$releases" | jq -r '.tag_name')
 
 install_and_update_vscodium() {
-  local tmpdir=$(mktemp -d)
-  trap 'rm -rf "$tmpdir"' RETURN
+  (
+    local tmpdir=$(mktemp -d)
+    trap 'rm -rf "$tmpdir"' RETURN
 
-  local assets=$(echo "$releases" | jq -r '.assets[]')
+    local assets=$(echo "$releases" | jq -r '.assets[]')
 
-  local bin_link=$(echo "$assets" \
-    | jq -r "select(.name == \"VSCodium-linux-x64-${latest_version}.tar.gz\") | .browser_download_url"
+    local bin_link=$(echo "$assets" \
+      | jq -r "select(.name == \"VSCodium-linux-x64-${latest_version}.tar.gz\") | .browser_download_url"
+    )
+    local checksum_link=$(echo "$assets" \
+      | jq -r "select(.name == \"VSCodium-linux-x64-${latest_version}.tar.gz.sha256\") | .browser_download_url"
+    )
+
+    cd "$tmpdir"
+
+    curl -fsSL -O "$bin_link"
+    curl -fsSL -O "$checksum_link"
+
+    sha256sum -c "VSCodium-linux-x64-${latest_version}.tar.gz.sha256"
+
+    local base_dir="${HOME}/.local"
+    local bin_dir="${base_dir}/bin"
+    local opt_dir="${base_dir}/opt/vscodium"
+
+    mkdir -p "$bin_dir"
+    mkdir -p "$opt_dir"
+
+    local current_dir="${opt_dir}/current"
+    local install_dir="${opt_dir}/${latest_version}"
+
+    mkdir -p "$install_dir"
+
+    tar -xzf "${tmpdir}/VSCodium-linux-x64-${latest_version}.tar.gz" -C "$install_dir"
+
+    ln -sfn "$install_dir" "$current_dir"
+    ln -sfn "${current_dir}/bin/codium" "${bin_dir}/codium"
   )
-  local checksum_link=$(echo "$assets" \
-    | jq -r "select(.name == \"VSCodium-linux-x64-${latest_version}.tar.gz.sha256\") | .browser_download_url"
-  )
 
-  cd "$tmpdir"
+  mkdir -p ~/.local/share/icons/hicolor/256x256/apps
 
-  curl -fsSL -O "$bin_link"
-  curl -fsSL -O "$checksum_link"
+  ln -sf \
+    ~/.local/opt/vscodium/current/resources/app/resources/linux/code.png \
+    ~/.local/share/icons/hicolor/256x256/apps/vscodium.png
 
-  sha256sum -c "VSCodium-linux-x64-${latest_version}.tar.gz.sha256"
+  mkdir -p ~/.local/share/applications
+  script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+  cp -f "${script_dir}/vscodium.desktop" ~/.local/share/applications/vscodium.desktop
 
-  local base_dir="${HOME}/.local"
-  local bin_dir="${base_dir}/bin"
-  local opt_dir="${base_dir}/opt/vscodium"
-
-  mkdir -p "$bin_dir"
-  mkdir -p "$opt_dir"
-
-  local current_dir="${opt_dir}/current"
-  local install_dir="${opt_dir}/${latest_version}"
-
-  mkdir -p "$install_dir"
-
-  tar -xzf "${tmpdir}/VSCodium-linux-x64-${latest_version}.tar.gz" -C "$install_dir"
-
-  ln -sfn "$install_dir" "$current_dir"
-  ln -sfn "${current_dir}/bin/codium" "${bin_dir}/codium"
+  update-desktop-database  ~/.local/share/applications/ 2> /dev/null || true
 }
 
 if [ "$current_version" = '0.0.0' ]; then
